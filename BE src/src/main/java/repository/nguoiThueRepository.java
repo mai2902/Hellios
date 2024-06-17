2a34070ff2f4d785b13ea8c9d4af729dbe6b3865
mai2902 <nguyenquynhmai465ap@gmail.com>
2024-06-17 15:29:31 +0700

    Delete BE src/src directory

--- BE src/src/main/java/repository/nguoiThueRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.nguoiThue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface nguoiThueRepository  extends JpaRepository<nguoiThue, Long> {
-}
--- BE src/src/main/java/repository/phongRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.phong;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface phongRepository  extends JpaRepository<phong, Long> {
-}
