fd0ce9c499843598070ededf365ba79d6f86926f
mai2902 <nguyenquynhmai465ap@gmail.com>
2024-06-18 11:54:08 +0700

    Delete BE source/src directory

--- BE source/src/main/java/repository/baoCaoNoRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.baoCaoNo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface baoCaoNoRepository  extends JpaRepository<baoCaoNo, Long> {
-}
