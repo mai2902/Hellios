21f6f97b180759ad0864064fc08fd93f81207df6
mai2902 <nguyenquynhmai465ap@gmail.com>
2024-06-19 10:43:12 +0700

    Delete BE source/src directory

--- BE source/src/main/java/app/repository/phongRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repository;
-
-import app.entity.phong;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface phongRepository  extends JpaRepository<phong, Long> {
-}
